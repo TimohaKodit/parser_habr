5a3ee25f679e8a72c06ef402db575dcc076d04a2
TimohaKodit <63901163+TimohaKodit@users.noreply.github.com>
2022-08-15 15:30:37 +0500

    Add files via upload

---
+++ parser_habr.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://habr.com/ru/flows/develop/"
+r = requests.get(url)
+src = r.text
+with open("index.html", "w", encoding = "utf-8-sig") as file:
+    file.write(src)
+with open("index.html", "r", encoding = "utf-8") as file:
+    soup = BeautifulSoup(file, "lxml")
+st = soup.find_all(class_="tm-article-snippet__title-link")
+for link in st:
+    part_of_link = link.get("href")
+
+
+for i in st:
+    print(i.text, f"https://habr.com{part_of_link}")
